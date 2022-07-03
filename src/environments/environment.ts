7f6ed8e5cb5a9cb35c0f275dbd8ae22a1d4fb1ad
Sergio <ser2004munoz@yahoo.com.ar>
2022-07-02 22:15:24 -0300

    SanCleServer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-production: false
+    production: false,
+    urlServer:'https://server-react-angular.herokuapp.com/'
+    
 };
 
 /*
